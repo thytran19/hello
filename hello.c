17dad508ba3409b4addbe74cea8cbea659e3888e
Thy <ttran55@huskers.unl.edu>
2022-08-25 14:09:51 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Thy Tran
+ *         ttran55@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Thy Tran, Electrical Engineering\n");
+  printf("Nga Pham, Solfware Engineeing\n");
+
+  return 0;
+}
